84b364acc9638f8b0bee633c464981ba7c4940ae
CreateNothing <inboxstuff@proton.me>
2026-02-01 21:32:20 +0100

    Fixed linting Error for Shadcn, just for CI Pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -26,4 +26,10 @@ export default tseslint.config(
             '@typescript-eslint/no-unused-vars': 'warn',
         },
     },
+    {
+        files: ['src/components/ui/**/*.tsx'],
+        rules: {
+            'react-refresh/only-export-components': 'off',
+        },
+    },
 );
